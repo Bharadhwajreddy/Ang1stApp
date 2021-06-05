4b61477062e968575e27d6e36ecc9d663daed84c
Bharahwajreddy <saibharadhwajreddy@gmaile.com>
2021-06-05 21:12:34 +0700

    addedp2c

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { productDetails } from 'src/models/productDetails';
+
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() productObject: productDetails={
+    productTitle:'its a titte',
+    Description:'hello hii lorem ipsum',
+    productImage:'https://image.shutterstock.com/image-photo/two-green-brinal-three-white-260nw-386559169.jpg'
+     }  
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { productDetails } from 'src/models/productDetails';
 
 @Component({
   selector: 'app-products',
@@ -7,5 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent  {
 
-products=[1,2,3,4,5,6]
+products:productDetails[] = [
+  {
+    productTitle:'brinjal',
+    Description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    productImage:'https://image.shutterstock.com/image-photo/two-green-brinal-three-white-260nw-386559169.jpg'
+  },
+  {
+    productTitle:'potato',
+    Description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    productImage:'https://image.shutterstock.com/image-photo/pile-potatoes-lying-on-wooden-600w-347630291.jpg'
+  },
+  {
+    productTitle:'ladyfinger',
+    Description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    productImage:'https://image.shutterstock.com/z/stock-photo-hibiscus-esculentus-indian-ladyfinger-indian-bhindi-and-ladyfinger-leaves-natural-ladyfinger-hd-1799046112.jpg'
+  },
+  {
+    productTitle:'tomato',
+    Description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    productImage:'https://image.shutterstock.com/image-photo/tomato-isolate-on-white-background-600w-1620914383.jpg'
+  },
+  {
+    productTitle:'onion',
+    Description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    productImage:'https://image.shutterstock.com/image-photo/full-frame-shot-purple-onions-600w-1284082165.jpg'
+  },
+  {
+    productTitle:'capsicum',
+    Description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    productImage:'https://image.shutterstock.com/image-photo/pile-green-capsicum-one-red-600w-1624645237.jpg'
+  }
+
+]
 }
